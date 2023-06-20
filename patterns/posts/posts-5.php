bf12141ccd03d76abc1dded009cb724e45661178
JiveDig <jivetheuniversal@gmail.com>
2023-06-20 15:00:31 -0400

    Add Block Types to post grid patterns

--- patterns/posts/posts-5.php
+++ patterns/posts/posts-5.php
@@ -3,6 +3,7 @@
  * Description: Simple posts grid
  * Categories: posts
  * Keywords: post, grid
+ * Block Types: acf/mai-post-grid
  */
 ?>
 <!-- wp:acf/mai-post-grid {"name":"acf/mai-post-grid","data":{"mai_post_grid_clone":{"mai_grid_block_show":["image","title"],"mai_grid_block_title_size":"md","mai_grid_block_image_orientation":"landscape","mai_grid_block_image_position":"full","mai_grid_block_align_text":"center","mai_grid_block_boxed":"0","mai_grid_block_columns":"3","mai_grid_block_columns_responsive":"0","mai_grid_block_align_columns":"start","mai_grid_block_align_columns_vertical":"","mai_grid_block_column_gap":"xl","mai_grid_block_row_gap":"xl","mai_grid_block_margin_top":"","mai_grid_block_margin_bottom":"","mai_grid_block_post_type":["post"],"mai_grid_block_query_by":"","mai_grid_block_posts_per_page":"6","mai_grid_block_posts_offset":"0","mai_grid_block_posts_date_after":"","mai_grid_block_posts_date_before":"","mai_grid_block_posts_orderby":"date","mai_grid_block_posts_order":"DESC","mai_grid_block_post_not_in":"","mai_grid_block_posts_exclude":"","mai_grid_block_disable_entry_link":"0"}},"mode":"preview"} /-->
